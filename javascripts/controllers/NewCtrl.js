fba093807cffa448b34710e41113f4b48eec290a
Jennifer Dutton <jduttondesign@gmail.com>
2016-12-07 14:28:59 -0600

    updated code

---
+++ javascripts/controllers/NewCtrl.js
@@ -0,0 +1,14 @@
+"use strict";
+
+app.controller("ItemNewCtrl", function($scope, $rootScope, $location, ItemFactory){
+  	$scope.newTask = {};
+
+	$scope.addNewItem = function(){
+	  $scope.newTask.isCompleted = false;
+	  $scope.newTask.uid = $rootScope.user.uid;
+	  ItemFactory.postNewItem($scope.newTask).then(function(itemId){
+	    $location.url("/items/list");
+	    $scope.newTask = {};
+	  });
+  	};
+});
