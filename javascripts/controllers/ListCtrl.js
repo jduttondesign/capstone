355b95c111a93ba538b377b273bbda4bb3b686e5
Jennifer Dutton <jduttondesign@gmail.com>
2016-12-07 14:24:36 -0600

    updated code

---
+++ javascripts/controllers/ListCtrl.js
@@ -0,0 +1,26 @@
+"use strict";
+
+app.controller("ItemListCtrl", function($scope, $rootScope, ItemFactory){
+  $scope.items = [];
+
+  let getItems = function(){
+    ItemFactory.getItemList($rootScope.user.uid).then(function(fbItems){
+      $scope.items = fbItems;
+    });
+  };
+  
+  getItems();
+
+  $scope.deleteItem = function(itemId){
+    ItemFactory.deleteItem(itemId).then(function(response){
+      getItems();
+    });
+  };
+
+  $scope.inputChange = function(thingy){
+    ItemFactory.editItem(thingy).then(function(response){
+      getItems();
+    })
+  };
+
+});
